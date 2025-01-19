c4dadeb200384aeb803e6aa1939eda66ea86002d
rohitdeepati <deepatirohit@gmail.com>
2025-01-19 12:51:52 +0530

    resume updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -81,7 +81,7 @@ const Navbar = () => {
               </li>
               <li className="nav-item mx-2 ">
                 <a
-                  href="https://drive.google.com/file/d/108h7PMEK4rkA8RhDWXaVqlKO20o70GQV/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1oAAWomsM3TPaVxuVhoT9SqKYOn7Emald/view?usp=sharing"
                   target="_blank"
                   className="text-white border-0 btn resumeBtn"
                 >
